1acfecd8db6731e8ff18064cedea8c9205924d80
PrinceATCHI <princeatchi324@gmail.com>
2026-03-06 20:50:27 +0100

    v4.1

--- config/cors.php
+++ config/cors.php
@@ -7,6 +7,9 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
+
+        'https://fahrradhauskauf.com',
+        'https://www.fahrradhauskauf.com',
         'http://localhost:5173',
         'http://127.0.0.1:5173',
         'https://*.ngrok-free.app', 
